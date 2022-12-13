94f693526732d2b93c9b88814ce34ea2d61dda09
Csandu <ces.pnw@gmail.com>
2022-12-12 21:25:41 -0800

    homepagextra

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>What's good in the neighborhood? Find honest reviews from the most trusted sources, your neighbors! </h1>
     </div>
   );
 };
