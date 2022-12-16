3d3989b7826becd65d2f1e1677249e7ae4fae17d
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-12-15 19:09:58 -0800

    fixed issues

--- src/App.js
+++ src/App.js
@@ -13,10 +13,15 @@ import Header from "./components/header/header.js";
 import Footer from "./components/footer/footer.js";
 
 import Signup from './pages/Signup';
-import Login from './pages/login';
+import Login from './pages/Login';
 import SingleQuestion from './pages/SingleQuestion';
 import Profile from './pages/Profile';
 import Map from "./components/Map/Map";
+
+// function App() {
+//   const [currentPage, setCurrentPage] = useState("Login");
+//   console.log(11, currentPage);
+
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -42,6 +47,7 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
 
 function App() {
   return (
@@ -79,9 +85,13 @@ function App() {
           </div>
           <Footer />
         </div>
-        <div>
+        {/* <div>
           <Map />
         </div>
+        <Footer />
+      </div> */}
+    {/* </Router> */}
+
       </Router>
     </ApolloProvider>
   );
