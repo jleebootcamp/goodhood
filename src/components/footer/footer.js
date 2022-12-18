93b12db1bed4de21800d3857dd87022dddc1921b
Joon Lee <jlee@alphalab.com>
2022-12-18 14:22:42 -0800

    cleanup

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,98 +8,83 @@ const Footer = () => {
   return (
     <div>
       <CFooter className="footer">
-        {data.map(({ url, contributor }) => (
-          <div key={url}>
-            <ul id="members">
-              <li>
-                <CLink
-                  href={url}
-                  alt="link to contributors GitHub Profile."
-                  target="blank"
-                >
-                  {contributor}
-                </CLink>
-              </li>
-            </ul>
-          </div>
-        ))}
-        <div id="copyright">
-          © 2022 Copyright:
-          <a> Team 7</a>{" "}
+      <CButton color="secondary" size="lg" onClick={() => setVisible(!visible)}>Contacts</CButton>
+      <CModal alignment="center" visible={visible} onClose={() => setVisible(false)}>
+        <CModalHeader onClose={() => setVisible(false)}>
+          <CModalTitle>Team Members</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <ul id="memberList">
+          <li>
+            <CLink
+              href="https://github.com/Bsayavong"
+              alt="link to contributors GitHub Profile."
+              target="blank"
+            >
+              Bryan Sayavong
+            </CLink>
+          </li>
+          <li>
+            <CLink
+              href="https://github.com/csandu123"
+              alt="link to contributors GitHub Profile."
+              target="blank"
+            >
+              Cosmin Sandu
+            </CLink>
+          </li>
+          <li>
+            <CLink
+              href="https://github.com/jleebootcamp"
+              alt="link to contributors GitHub Profile."
+              target="blank"
+            >
+              Joon Lee
+            </CLink>
+          </li>
+          <li>
+            <CLink
+              href="https://github.com/mncole98"
+              alt="link to contributors GitHub Profile."
+              target="blank"
+            >
+              Matthew Cole
+            </CLink>
+          </li>
+          <li>
+            <CLink
+              href="https://github.com/sheetaljwl795"
+              alt="link to contributors GitHub Profile."
+              target="blank"
+            >
+              Sheetal Jawale
+            </CLink>
+          </li>
+          <li>
+            <CLink
+              href="https://github.com/snovelli1021"
+              alt="link to contributors GitHub Profile."
+              target="blank"
+            >
+              Stephen Novelli
+            </CLink>
+          </li>
+          </ul>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="secondary" onClick={() => setVisible(false)}>
+            Close
+          </CButton>
+        </CModalFooter>
+      </CModal>
+      <div id="copyright">
+        © 2022 Copyright:
+        <a> Team 7 </a>{" "}
+        {/* Wallpaper Source Citation: StarLineArts, "World map with circular particles background", iStockPhoto.com, April 06, 2019 */}
+        <a href="https://www.istockphoto.com/vector/world-map-with-circular-particles-background-gm1140757772-305381386">(Wallpaper Image Source)</a>
         </div>
       </CFooter>
     </div>
   );
 };
-
-// const Footer = () => {
-//   return (
-//     <div>
-//       <CFooter>
-//         <div className="footer">
-//           <ul id="members">
-//             <li>
-//               <CLink
-//                 href="https://github.com/Bsayavong"
-//                 alt="link to contributors GitHub Profile."
-//                 target="blank"
-//               >
-//                 Bryan Sayavong
-//               </CLink>
-//             </li>
-//             <li>
-//               <CLink
-//                 href="https://github.com/csandu123"
-//                 alt="link to contributors GitHub Profile."
-//                 target="blank"
-//               >
-//                 Cosmin Sandu
-//               </CLink>
-//             </li>
-//             <li>
-//               <CLink
-//                 href="https://github.com/jleebootcamp"
-//                 alt="link to contributors GitHub Profile."
-//                 target="blank"
-//               >
-//                 Joon Lee
-//               </CLink>
-//             </li>
-//             <li>
-//               <CLink
-//                 href="https://coreui.io"
-//                 alt="link to contributors GitHub Profile."
-//                 target="blank"
-//               >
-//                 Matthew Cole
-//               </CLink>
-//             </li>
-//             <li>
-//               <CLink
-//                 href="https://github.com/sheetaljwl795"
-//                 alt="link to contributors GitHub Profile."
-//                 target="blank"
-//               >
-//                 Sheetal Jawale
-//               </CLink>
-//             </li>
-//             <li>
-//               <CLink
-//                 href="https://github.com/snovelli1021"
-//                 alt="link to contributors GitHub Profile."
-//                 target="blank"
-//               >
-//                 Stephen Novelli
-//               </CLink>
-//             </li>
-//           </ul>
-//           <div id="copyright">
-//             © 2022 Copyright:
-//             <a> Team 7</a>
-//           </div>
-//         </div>
-//       </CFooter>
-//     </div>
-//   );
-// };
 export default Footer;
